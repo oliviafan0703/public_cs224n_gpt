7e6eac8770f9cb93a58e2c9544c469a0013d0b81
Yisi Lyu <yisi_lyu@umail.ucsb.edu>
2025-02-26 03:00:33 -0800

    updates

--- config.py
+++ config.py
@@ -153,7 +153,7 @@ class PretrainedConfig(object):
       )
 
     try:
-      # Load from URL or cache if already cached
+      # Load from URL or cache if aeady cached
       resolved_config_file = cached_path(
         config_file,
         cache_dir=cache_dir,
@@ -196,7 +196,8 @@ class GPT2Config(PretrainedConfig):
           num_attention_heads=12,
           intermediate_size=3072,
           hidden_act="gelu",
-          hidden_dropout_prob=0.1,
+          # hidden_dropout_prob=0.1,
+          hidden_dropout_prob=0.3,
           attention_probs_dropout_prob=0.1,
           max_position_embeddings=1024,
           type_vocab_size=2,
